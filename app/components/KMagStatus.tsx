e6992998e4c19b0e6c48ba8b28bbb0307b2f2759
Huntly Cameron <huntly.cameron@gmail.com>
2026-06-14 07:57:04 +0100

    retirement logic fix

--- app/components/KMagStatus.tsx
+++ app/components/KMagStatus.tsx
@@ -8,17 +8,16 @@ export const KMagStatus = (props: {
     const num = props.num;
 
     let span = <span className="text-red-600">No</span>;
-    if (parseInt(cars[0].carNumber, 10) == num) {
-        if (cars[0].overallPosition == 1) {
-            span = <span className="text-emerald-400">YES!</span>;
-        } else {
-            span = <span className="text-yellow-300">Sort of...</span>;
-        }
-    }
-
     cars.forEach((car) => {
-        if (!car.isRunning) {
-            span = <span className="text-red-300">He&apos;s gone home</span>;
+        if (parseInt(car.carNumber, 10) == num) {
+            if (!car.isRunning) {
+                span = <span className="text-red-300">He&apos;s gone home</span>;
+            } else if (car.overallPosition == 1) {
+                span = <span className="text-emerald-400">YES!</span>;
+            } else if (car.position == 1) {
+                // class pos
+                span = <span className="text-yellow-300">Sort of...</span>;
+            }
         }
     });
 
